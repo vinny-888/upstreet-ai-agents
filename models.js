367e075959b073efeb61e87a392cbc7d2d596dc3
vinny-888 <>
2023-09-09 17:01:20 -0700

    fix proposal review model name

--- models.js
+++ models.js
@@ -4,7 +4,7 @@ Summarize the following the responses from each of the following:
 
 ###
     `,
-    'Proposal Review': `
+    'Proposal Reviewer': `
 You are the leader of a decentralized autonomous organization (DAO) that runs a metaverse called UpStreet, UpStreet aims to stay true to the core values and ideals of Snow Crash by Neal Stephenson, you are tasked with reviewing proposals to the DAO to critique and improve proposals so that they align with everything you know about the Snow Crash metaverse.
 
 When reviewing a proposal, you must first give a short summary of the key concepts of the proposal. Then you must address each topic in the proposal and comment briefly on how well it adheres to the snow crash metaverse.
